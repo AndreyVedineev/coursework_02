271bff605064c1ac5f1ab4f0628888feb4fdf432
AVedineev <119727031+avedineev@users.noreply.github.com>
2023-02-17 10:10:49 +0300

    02

--- basicword.py
+++ basicword.py
@@ -4,7 +4,7 @@ class BasicWord:
         self.set_subwords = set_subwords
 
     def __repr__(self):
-        return f'слово: {self.original_word} набор допустимых подслов {self.set_subwords}'
+        return f'Слово: {self.original_word} набор допустимых подслов {", ".join(self.set_subwords)})'
 
     def checks_entered_word(self, user_answer):
         """проверку введенного слова в списке допустимых подслов (вернет bool),"""
@@ -12,9 +12,9 @@ class BasicWord:
             return True
         return False
 
-    def counts_number_subwords(self):
+    def counts_subwords(self):
         """подсчет количества подслов (вернет int)."""
-        pass
+        return int(len(self.set_subwords))
 
 
 """При инициализации ** экземпляру задаются: ** исходное слово ** и набор ** допустимых слов,
---
+++ player.py
@@ -0,0 +1,32 @@
+class Player:
+    def __init__(self, name_user, words_used):
+        self.name_user = name_user  # имя пользователя
+        self.words_used = words_used  # использованные слова пользователя - list()
+
+    def __repr__(self):
+        return f'Пользователь: {self.name_user},\n' \
+               f'Использованные слова: ({", ".join(self.words_used)})'
+
+    def get_number_words_used(self):
+        """Получение количества использованных слов (возвращает int)"""
+        return int(len(self.words_used))
+
+    def add_word_to_used_words(self, user_answer=None):
+        """Добавление слова в использованные слова (ничего не возвращает)"""
+        self.words_used.append(user_answer)
+
+    def check_word_before(self, user_answer=None):
+        """Проверка использования данного слова до этого (возвращает True, если слово не использовалось,
+        можно добавлять!)."""
+        if user_answer in self.words_used:
+            return False
+        return True
+
+#
+# word = ["пони", "тон", "ион", "опт", "пот", "топ", "пион", "тип"]
+# pl_1 = Player("Vasay", word)
+# print(pl_1)
+# print(pl_1.get_number_words_used())
+#
+# print(pl_1)
+# print(pl_1.check_word_before("тип"))
