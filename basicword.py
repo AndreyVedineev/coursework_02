b3b6670e23f3b32aa4e67a35e9aa4960c9dcd407
AVedineev <119727031+avedineev@users.noreply.github.com>
2023-02-17 09:21:55 +0300

    01

--- basicword.py
+++ basicword.py
@@ -4,11 +4,13 @@ class BasicWord:
         self.set_subwords = set_subwords
 
     def __repr__(self):
-        pass
+        return f'слово: {self.original_word} набор допустимых подслов {self.set_subwords}'
 
-    def checks_entered_word(self):
+    def checks_entered_word(self, user_answer):
         """проверку введенного слова в списке допустимых подслов (вернет bool),"""
-        pass
+        if user_answer in self.set_subwords:
+            return True
+        return False
 
     def counts_number_subwords(self):
         """подсчет количества подслов (вернет int)."""
@@ -17,3 +19,4 @@ class BasicWord:
 
 """При инициализации ** экземпляру задаются: ** исходное слово ** и набор ** допустимых слов,
 составленных из исходного."""
+
---
+++ test.py
@@ -0,0 +1,3 @@
+# Your JSON is hosted at: https://jsonkeeper.com/b/7SGM
+
+"
